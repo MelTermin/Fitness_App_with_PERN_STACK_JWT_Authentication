5096f97412a0dff0eb1725615002f53c228da286
Selin Termin <80844457+MelTermin@users.noreply.github.com>
2021-12-15 21:40:55 +0200

    new css

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,17 +1,33 @@
-import React from 'react'
+import React ,{useRef,useEffect}from 'react'
+import { init } from 'ityped'
 import { Link } from "react-router-dom";
 
 function Landing() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed:60,
+      strings: ["Sign In and start your exercise"]
+    });
+  }, []);
   return (
     <div className="landing-container">
-    <h1>Welcome to Your Fitness App</h1>
-    <p>Sign In and start your exercise</p>
-    <Link to="/login" className="login-btn">
-      Login
-    </Link>
-    <Link to="/register" className="register-btn">
-      Register
-    </Link>
+      <div className="landing-text">
+          <h1>Welcome to Your Fitness App</h1>
+          <br></br>
+          <h2 className="text-ref" ref= {textRef}></h2>
+          <div className="links">
+              <Link to="/login" className="login-btn">
+                Login
+              </Link>
+              <Link to="/register" className="login-btn">
+                Register
+              </Link>
+          </div>
+    </div>
   </div>
   )
 }
