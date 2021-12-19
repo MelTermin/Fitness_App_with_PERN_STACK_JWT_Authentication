07fa2685cee6cc7f6710ca8097ed6f0324eacf3a
Selin Termin <80844457+MelTermin@users.noreply.github.com>
2021-12-19 11:09:50 +0200

    new changes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express=require("express");
 const app=express();
 const cors=require("cors");
+const env=require('dotenv')
+env.config();
 
 app.use(cors());
 app.use(express.json());//this code allows me to access req.body
@@ -11,6 +13,6 @@ app.use("/auth", require("./routes/jwtAuth"));
 app.use("/dashboard",require("./routes/dashboard"))
 
 
-app.listen(4000,()=> {
-  console.log("the server is running on port 4000")
+app.listen(process.env.PORT,()=> {
+  console.log(`the server is running on port` + process.env.PORT)
 })
