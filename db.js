209e29449a0ecfa7453993fde250863190324532
Selin Termin <80844457+MelTermin@users.noreply.github.com>
2021-12-19 13:12:47 +0200

    changes

--- db.js
+++ db.js
@@ -8,7 +8,7 @@ const pool= new Pool({
   password:process.env.DB_PASSWORD,
   port:process.env.DB_PORT,
   database:process.env.DB_DATABASE,
-  ssl:{rejectUnauthorized:false},
+  ssl:{rejectUnauthorized:true},
  
 });
 
