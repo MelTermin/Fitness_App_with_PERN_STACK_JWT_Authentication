2bfabcca9bad95441912ffeb1e15a0ebed8ed95e
Selin Termin <80844457+MelTermin@users.noreply.github.com>
2021-12-19 11:55:38 +0200

    changes

--- server/index.js
+++ server/index.js
@@ -8,6 +8,11 @@ app.use(cors());
 app.use(express.json());//this code allows me to access req.body
 
 //register & login routes//
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+});
 
 app.use("/auth", require("./routes/jwtAuth"));
 app.use("/dashboard",require("./routes/dashboard"))
